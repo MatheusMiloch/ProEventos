6c125460396a90433cdaff47237899509615fea4
Matheus Miloch <matheus.miloch@targetwork.com.br>
2022-05-23 23:52:33 -0300

    Adicionando EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -10,41 +11,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[] {
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular 11 e .NET 5 ",
-                    Local = "Belo Horizonte",
-                    Lote = "1° Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString(),
-                    ImagemUrl = "foto.png"
-                },
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular e testes ",
-                    Local = "Rio de Janeiro",
-                    Lote = "2° Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(4).ToString(),
-                    ImagemUrl = "foto.png"
-                },
-        };
+        private readonly DataContext _context;
 
-        public EventoController()
+        public EventoController(DataContext context)
         {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(x => x.EventoId == id); 
+            return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
         }
     }
 }
